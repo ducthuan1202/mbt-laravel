71b6f81390446ad4b69437dfcf1e20199644e568
Nguyen Duc Thuan <ducthuan1202@gmail.com>
2018-12-14 08:15:09 +0700

    update code

--- database/migrations/2018_12_10_023530_create_users_table.php
+++ database/migrations/2018_12_10_023530_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('role_id');
+            $table->string('role');
             $table->string('name');
             $table->string('mobile');
             $table->string('email')->nullable();
